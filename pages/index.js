0ce8c94ebd3279bbca5f080c94d7072b9b08f15f
Eriza Zehezkiel Gracia <nftrgaming@gmail.com>
2022-11-12 17:25:08 +0700

    Editing things

--- pages/index.js
+++ pages/index.js
@@ -10,10 +10,10 @@ export default function Home() {
         <meta charSet='UTF-8'/>
         <meta name="viewport" content="width=device-width,minimum-scale=1,initial-scale=1" />
         {/* OpenGraph */}
-        <meta property="og:site_name" content="Eriza Zehezkiel Gracia" />
-        <meta property="og:title" content="Eriza Zehezkiel Gracia" />
+        <meta property="og:site_name" content="Senopati SMANSA" />
+        <meta property="og:title" content="Senopati SMANSA" />
         <meta property="og:type" content="website" />
-        <meta property="og:description" content="Personal Website of Eriza Zehezkiel Gracia." />
+        <meta property="og:description" content="Web Tugas Eriza Zehezkiel Gracia." />
         <meta property="og:url" content="https://erizazg.my.id" />
         <meta property="og:image" content="http://raw.githubusercontent.com/Eriza-Z/Eriza-Z.github.io/master/Web/Icon/icon.webp" />
         <meta property="og:image:secure_url" content="https://raw.githubusercontent.com/Eriza-Z/Eriza-Z.github.io/master/Web/Icon/icon.webp" />
@@ -26,19 +26,19 @@ export default function Home() {
         <meta name="twitter:card" content="summary" />
         <meta name="twitter:site" content="@eriza_gracia" />
         <meta name="twitter:creator" content="@eriza_gracia" />
-        <meta name="twitter:url" content="https://erizazg.my.id" />
-        <meta name="twitter:title" content="Eriza Zehezkiel Gracia" />
-        <meta name="twitter:description" content="Personal Website of Eriza Zehezkiel Gracia." />
+        <meta name="twitter:url" content="https://senopati.erizazg.my.id" />
+        <meta name="twitter:title" content="Senopati SMANSA" />
+        <meta name="twitter:description" content="Web Tugas Eriza Zehezkiel Gracia." />
         <meta name="twitter:image" content="https://raw.githubusercontent.com/Eriza-Z/Eriza-Z.github.io/master/Web/Icon/icon.webp" />
         <meta name="twitter:wigets:theme" content="dark" />
         {/* Apple */}
         <link rel="apple-touch-icon" href="../public/apple-touch-icon.png" />
         {/* Canonical */}
-        <link rel="canonical" href="https://erizazg.my.id" />
+        <link rel="canonical" href="https://senopati.erizazg.my.id" />
         {/* Website Thingy */}
-        <title>Eriza Zehezkiel Gracia</title>
-        <meta name="keywords" content="Eriza Zehezkiel, Eriza Z, Eriza Zehezkiel Gracia, EZGS, ezgs" />
-        <meta name="description" content="Personal Website of Eriza Zehezkiel Gracia where this Web page Layout is like a Portofolio Website Page Layout" />
+        <title>Senopati SMANSA</title>
+        <meta name="keywords" content="Senopati SMANSA" />
+        <meta name="description" content="Web Tugas Eriza Zehezkiel Gracia" />
         <meta name="author" content="Eriza Zehezkiel Gracia" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
