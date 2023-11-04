35e6cfef120b479ecd3252b2d44616d86f31f901
Eriza Zehezkiel Gracia <erz@erizazg.my.id>
2023-11-04 17:42:20 +0700

    tired

--- next.config.js
+++ next.config.js
@@ -4,7 +4,6 @@ const nextConfig = {
     domains: ['cdn.discordapp.com', 'mergoreb.sirv.com', 's6.imgcdn.dev'],
     unoptimized: true,
     },
-    output: 'export',
 }
 
 module.exports = nextConfig
