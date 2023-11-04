4cddd82ece8be2594229f7875df12d48c55efb92
Eriza Zehezkiel Gracia <erz@erizazg.my.id>
2023-11-04 17:38:39 +0700

    Editing config

--- next.config.js
+++ next.config.js
@@ -1,11 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    reactStrictMode: true,
-  swcMinify: true,
   images: {
     domains: ['cdn.discordapp.com', 'mergoreb.sirv.com', 's6.imgcdn.dev'],
     unoptimized: true,
     },
+    output: 'export',
 }
 
 module.exports = nextConfig
