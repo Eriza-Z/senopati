44e1c40f6eb290b2a93a9e666f319ef9fac645b6
Eriza Zehezkiel Gracia <erz@erizazg.my.id>
2023-11-09 11:57:55 +0700

    Another update

--- app/page.js
+++ app/page.js
@@ -1,20 +1,12 @@
-import Footer from "@/components/other/Footer";
-import Image from "next/image";
-import Link from "next/link";
+import Head from "next/head";
+import Link from "@/components/pages/link"
+import Footer from '@/components/other/Footer'
 
-export default function Home() {
+export default function link() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between pt-8 mx-16">
-      <h1 className="font-bold">Senopati SMANSA Medan</h1>
-      <Image src="https://ik.imagekit.io/erz/head.jpg?updatedAt=1690815901380"
-            alt="Senopati SMANSA"
-            className="pt-2 rounded-full"
-            id="HeroImage"
-            width={225}
-            height={225}/>
-      <h2>Check our socmed and song playlist at:<br/></h2>
-      <p className="font-mono font-thin"><Link href={"/link"}> HERE!</Link></p>
-      <Footer />
-    </main>
+    <div>
+      <Link/>
+      <Footer/>
+    </div>
   );
 }
