4d5dce9e0fc74f2212dd312497c68eebdfbfb74d
Eriza Zehezkiel Gracia <erz@erizazg.my.id>
2023-11-23 20:58:12 +0700

    bazaar update

--- app/link/page.js
+++ app/link/page.js
@@ -1,10 +1,11 @@
+import Link from "@/components/pages/link"
 import Footer from '@/components/other/Footer'
-import Bazar from "@/components/pages/bazar";
 
 export default function link() {
   return (
     <main>
-      <Bazar/>
+      <Link/>
+      <Footer/>
     </main>
   );
 }
