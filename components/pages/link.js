0edb10af357d204e6abf4a132f49d24665a85cf5
Eriza Zehezkiel Gracia <erz@erizazg.my.id>
2024-01-20 17:45:20 +0700

    Updating background, fixing hero image, and updating NextJS to the newest version

--- components/pages/link.js
+++ components/pages/link.js
@@ -4,17 +4,17 @@ import Image from "next/image";
 function Link() {
   return (
     <section className={'content flex min-h-screen flex-col items-center justify-between pt-8 mx-16'} id='link-main'>
-        <div className="top-picture mb-3">
+        <div className="top-picture mb-1">
           <Image
-            src="https://ik.imagekit.io/erz/head.jpg?updatedAt=1690815901380"
+            src="https://ik.imagekit.io/erz/Senopati/head.jpg?updatedAt=1700715395048"
             alt="Senopati SMANSA"
             width={180}
             height={180}
             className="rounded-full"
           />
         </div>
-        <h1 className='my-3'>Senopati SMANSA</h1>
-        <h2>Social Media</h2>
+        <h1 className='my-1 font-bold text-white'>Senopati SMANSA</h1>
+        <h2 className='font-semibold text-white'>Social Media</h2>
         <div className={'button_area'}>
           <a
             href="https://instagram.com/senopatismansa/"
@@ -25,7 +25,7 @@ function Link() {
             Instagram
           </a>
         </div>
-        <h2>Playlist</h2>
+        <h2 className='font-semibold text-white'>Playlist</h2>
         <div className='button_area'>
           <a href='https://l.instagram.com/?u=https%3A%2F%2Fopen.spotify.com%2Fplaylist%2F5htaEm7mbbMs5DP3y54CAj%3Fsi%3DxLu0GhjqS9-VX-XGi2O3pw%26utm_source%3Dcopy-link&e=AT313TocokaxkrrfI73Y3qD7qsGiLPpNp18Xjqj6OS7E9e_GTrVXwcE7C1aDu6L9Eoa1LcUERxkl7HbMyNBvKSCf59D9w7lLfbwWSFA' className={'a'} rel='noopenner noreferrer'>
             <Image src="https://www.svgrepo.com/show/475684/spotify-color.svg" alt="Spotify Playlist" width={25} height={25} className='justify-center' />Spotify Playlist</a>
